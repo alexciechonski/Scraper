85c7bf211cad3e5283a8b70b5d33a4af547454e2
alexciechonski <101641507+alexciechonski@users.noreply.github.com>
2023-08-17 20:23:12 +0100

    stations.py

--- stations.py
+++ stations.py
@@ -3,10 +3,23 @@ from selectolax.parser import HTMLParser
 from bs4 import BeautifulSoup
 import time
 
-# open the website
-# search "trafostacja"
-# get all the trafostacje addresses
-# create a json with nr - address
+'''
+I'm trying to scrape the adresses of all electrical transformer stations in Poland from a map.
+
+The map is dynamically loaded and to get an address of a given station the user has to hover their mouse over
+an icon respresenting a station, then wait until a balloon(?) pops up and then click "more", which redirects
+them to a new page in a new window in their browser.
+
+The number of stations to be scraped is in the hundreds, so using a web crawler will be very time-indefficient(?).
+
+There are no <a> tags in the html so I couln't find any links.
+
+I tried looking through the requests the website sends to the server, but I didn't find  any that would lead
+me to the appropriate website.
+
+I am mainly looking to understand how the website sends requests to the server to redirect the user to the new window
+and find the best way to scrape the website.
+'''
 
 url = 'https://mapa.targeo.pl/trafostacja,26,21.3768641,52.0938685?data=eyJmdHMiOnsicSI6InRyYWZvc3RhY2phIn0sIndpbiI6InNlYXJjaC1mb3JtIn0='
 
